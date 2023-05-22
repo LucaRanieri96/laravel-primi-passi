457ed211a4433d9696cdb778871c33382a34f0c7
LucaRanieri96 <luca.ranieri.4@gmail.com>
2023-05-22 14:19:40 +0200

    Add Hello World with variables

--- laravel-primi-passi/routes/web.php
+++ laravel-primi-passi/routes/web.php
@@ -14,5 +14,8 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('homepage');
+
+    $message = "Hello World!";
+
+    return view('homepage', compact('message'));
 });
